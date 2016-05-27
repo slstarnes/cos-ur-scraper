6f2568a320478f80465bde21137440a2d593805e
Starnes, Luke <luke.starnes@gtri.gatech.edu>
2016-05-27 15:37:21 -0400

    added sorting by date before dumping csv file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from IPython.display import display
 import re
 import pandas as pd
 import os
+import datetime as dt
 
 req = requests.get('http://consequenceofsound.net/upcoming-releases/')
 soup = BeautifulSoup(req.text, 'html.parser')
@@ -15,7 +16,7 @@ class Album():
         self.album_title = album_title
     def __str__(self):
         return '%s - %s (%s)'%(self.artist, self.album_title, self.date)
-		
+
 def strip_non_ascii(string):
     ''' Returns the string without non-ASCII characters'''
     stripped = (c for c in string if 0 < ord(c) < 127)
@@ -24,26 +25,26 @@ def strip_non_ascii(string):
 def date_cleaner(date):
     return re.sub(r'\({1}.*\){1}','',date).strip()
 
-	
+
 albums = []
 article = soup.find(id="main-content")
-for e1 in article.find_all('section', class_ = 'results-chunk'):        
+for e1 in article.find_all('section', class_ = 'results-chunk'):
     for e2 in e1.find_all('h2'):
         date = date_cleaner(e2.text)
         for e3 in e1.find_all('span', class_ = 'title'):
             s = e3.text.replace(u'\u2013','-')
             s = s.replace(u'\xa0',' ')
-            s = s.split(" - ") 
+            s = s.split(" - ")
             artist = strip_non_ascii(s[0])
             album_title = strip_non_ascii(s[1])
             albums.append(Album(date,artist,album_title))
-			
-			
+
 if os.path.exists('albums.csv'):
     old_albums_df = pd.read_csv('albums.csv')
-    display (old_albums_df)
 else:
     old_albums_df = pd.DataFrame()
+
+prev_size = len(old_albums_df)
 
 albums_df = pd.DataFrame()
 for i, a in enumerate(albums):
@@ -53,5 +54,12 @@ for i, a in enumerate(albums):
 
 albums_df = pd.concat([old_albums_df,albums_df])
 albums_df.drop_duplicates(subset = ['Artist','Title'],inplace=True)
-display(albums_df)
+albums_df['Date'] = albums_df['Date'].apply(lambda x:
+                                            dt.datetime.strptime(x,"%B %d, %Y"))
+albums_df.sort_values('Date',inplace=True)
+albums_df['Date'] = albums_df['Date'].apply(lambda x:
+                                            x.strftime("%B %d, %Y"))
+
+print("Number of rows added: %i"%(prev_size - len(albums_df)))
+
 albums_df.to_csv('albums.csv',index=False)
